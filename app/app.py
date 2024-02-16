3217268b9c6f5cd8f1b653591697242b6cef5262
Zongdong Yu <yzd@Zongdongs-MacBook-Pro.local>
2024-02-16 21:57:33 +0800

    fix the ui selection option

--- app/app.py
+++ app/app.py
@@ -21,8 +21,16 @@ def run_simulation():
     # Convert the DataFrame to an HTML table
     table_html = results.variables.OHM.to_html(classes='table table-striped', index=False)
 
+    
+    if market_condition == 'bull':
+        explanation_txt =  'This is for bull market'
+    elif market_condition == 'bear':
+        explanation_txt = 'This is for bear market'
+    else :
+        explanation_txt = 'This is for base market'
+
     # Return the 'result.html' template with the HTML table
-    return render_template('index.html', table_html=table_html)
+    return render_template('index.html', table_html=table_html, explanation_txt=explanation_txt)
 
 
 if __name__ == '__main__':
